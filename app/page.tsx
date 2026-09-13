52ee17f7ad5d16a6b2bade9495208f603f374d47
tonykone555 <tonykone555@gmail.com>
2026-09-14 01:39:22 +0200

    Mount saved-product notebook

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import LuminaWorld from "@/components/lumina/LuminaWorld";
 import YnotDrawer from "@/components/lumina/YnotDrawer";
 import YnotIntentBridge from "@/components/lumina/YnotIntentBridge";
 import SubcategoryNavigator from "@/components/lumina/SubcategoryNavigator";
+import SavedNotebook from "@/components/lumina/SavedNotebook";
 
 export default function Home() {
-  return <><LuminaWorld/><SubcategoryNavigator/><YnotDrawer/><YnotIntentBridge/></>;
+  return <><LuminaWorld/><SubcategoryNavigator/><YnotDrawer/><YnotIntentBridge/><SavedNotebook/></>;
 }
