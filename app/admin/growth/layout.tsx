7f9a9fb155d4898a3c86c4dcfd821f1b861c4b73
tonykone555 <tonykone555@gmail.com>
2026-09-23 17:33:56 +0200

    Load viewport fix for create-from-ad workflow

--- app/admin/growth/layout.tsx
+++ app/admin/growth/layout.tsx
@@ -1,4 +1,5 @@
 import AdminSessionBridge from "@/components/admin/AdminSessionBridge";
+import "./research-modal-viewport-fix.css";
 
 export default function GrowthAdminLayout({children}:{children:React.ReactNode}){
  return <><AdminSessionBridge/>{children}</>;
